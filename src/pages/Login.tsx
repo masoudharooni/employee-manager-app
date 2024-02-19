0b08eb2fc524b181286f8bb59f7b833f45841e1e
Masoud Harooni <masoudharooni50@gmail.com>
2024-02-19 20:20:06 +0330

    [ADD] react-router-dom

---
+++ src/pages/Login.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+    return <section className="bg-green-300">This is Login page!</section>;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function HomePage() {
+    return <section className="bg-yellow-500">
+        This is home page!
+    </section>
+}
