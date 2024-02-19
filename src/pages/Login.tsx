0454ace2b6fba27138776d923ecd25d1f1b74581
Masoud Harooni <masoudharooni50@gmail.com>
2024-02-19 20:27:01 +0330

    [ADD] material-ui | react-icons | ant-desing

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@mui/material";
+import { AiFillAudio } from "react-icons/ai";
+
 export default function LoginPage() {
-    return <section className="bg-green-300">This is Login page!</section>;
+    return <section className="bg-green-300">
+        <Button color="warning">
+            <AiFillAudio />
+        </Button>
+    </section>;
 }
